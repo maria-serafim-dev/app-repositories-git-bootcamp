8f8347f67f49278cd5752c38dbc7c5db1c61600b
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-07-20 20:42:12 -0300

    Abrir url do repositório a partir do click no card

--- app/src/main/java/com/example/portfoliogithub/ui/RepoListAdapter.kt
+++ app/src/main/java/com/example/portfoliogithub/ui/RepoListAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.portfoliogithub.ui
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -17,18 +20,27 @@ class RepoListAdapter : ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallba
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position), holder.itemView.context)
     }
 
     inner class ViewHolder(
         private val binding: ItemRepoBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Repo) {
+        fun bind(item: Repo, context: Context) {
             binding.tvRepoName.text = item.name
             binding.tvRepoDescription.text = item.description
             binding.tvRepoLanguage.text = item.language
             binding.cpStar.text = item.stargazersCount.toString()
+            binding.root.setOnClickListener {
+                clickRepository(item.htmlURL, context)
+            }
+        }
+
+        private fun clickRepository(htmlURL: String, context: Context){
+            val uri = Uri.parse(htmlURL)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            context.startActivity(intent)
         }
     }
 }
